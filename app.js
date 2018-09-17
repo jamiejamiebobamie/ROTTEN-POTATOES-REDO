d3ecc6770ea4759731e920ae8fb53ac57406e4dd
jamiejamiebobamie <jmccrory@vt.edu>
2018-09-17 12:04:34 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+const Review = mongoose.model('Review', {
+  title: String,
+  description: String,
+  movieTitle: String
+});
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -9,14 +23,35 @@ app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
 
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" },
-  { title: "Poopy Butthole" },
-  { title: "Gay Bottom" }
-]
+//let reviews = [
+//  { title: "Great Review" },
+//  { title: "Next Review" },
+//  { title: "Poopy Butthole" },
+//  { title: "Gay Bottom" }
+//]
 
 // INDEX
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
