fe78122daa26f4eb56d188562fb0b2b0d8862f22
jamiejamiebobamie <jmccrory@vt.edu>
2018-09-17 11:16:55 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
-})
-
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
+})
+
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" },
+  { title: "Poopy Butthole" },
+  { title: "Gay Bottom" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
 })
